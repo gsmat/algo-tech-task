a7b0889b6ca4a82386f5cdf6a36aafa98009dc71
gsmat <106599109+gsmat@users.noreply.github.com>
2022-12-11 21:21:00 +0400

    Update response format

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -3,22 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\JsonResponse;
+use JsonException;
 
 class BaseController extends Controller
 {
     /**
      * @param bool|string|int $message
      * @param array|string|null $data
-     * @param string $dataKeyword
+     * @param string|null $dataKeyword
      * @param int $status
      * @return JsonResponse
+     * @throws JsonException
      */
 
-    public static function responseJson(bool|string|int $message, array|null|string $data, string $dataKeyword, int $status): JsonResponse
+    public static function responseJson(bool|string|int $message, array|null|string $data, string|null $dataKeyword, int $status): JsonResponse
     {
+        if ($data === null) {
+            return response()->json([
+                'message' => $message,
+            ], $status);
+        }
+        if (is_string($data)) {
+            $data = json_decode($data, false, 512, JSON_THROW_ON_ERROR);
+        }
         return response()->json([
             'message' => $message,
-            $dataKeyword => json_decode($data)
+            $dataKeyword => $data
         ], $status);
+
     }
 }
