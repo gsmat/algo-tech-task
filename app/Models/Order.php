d8bd95d84183c55275b9e486c6c09c581601cb2b
gsmat <106599109+gsmat@users.noreply.github.com>
2022-12-11 14:12:00 +0400

    adding Models

---
+++ app/Models/Order.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function bond(): BelongsTo
+    {
+        return $this->belongsTo(Bond::class, 'bond_id', 'id');
+    }
+
+}
